e60e38402eb300254274c29673ff0e71004f1fea
Your Name <you@example.com>
2021-12-06 15:30:15 +0100

    Functional game lacking the export of the not know list of states

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,7 +1,6 @@
 import turtle
 import pandas as pd
 
-
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -10,15 +9,21 @@ turtle.shape(image)
 
 ds_states = pd.read_csv("50_states.csv")
 all_states = ds_states.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the state" , prompt="Whats another state name?").lower()
-
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = ds_states[ds_states.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 of the state",
+                                    prompt="Whats another state name?").title()
+    print(answer_state)
+    if answer_state == "Exit":
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = ds_states[ds_states.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
 
 screen.exitonclick()
