bb034791a0913d259574702b102b4a4a9ed377fc
Your Name <you@example.com>
2021-12-06 15:49:47 +0100

    Done with the auto export to .csv, working on a confirmation block for saving

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,4 +1,6 @@
 import turtle
+
+import pandas
 import pandas as pd
 
 screen = turtle.Screen()
@@ -14,8 +16,15 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 of the state",
                                     prompt="Whats another state name?").title()
-    print(answer_state)
+    # print(answer_state)
+
     if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
